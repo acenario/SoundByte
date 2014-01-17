5813156b3b7fd9cc64d430da94ac9a89174d968f
Arjun Bhatnagar <me@arjunb.com>
2014-01-17 15:29:19 -0500

    Initial Commit

---
+++ SoundByte/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SoundByte
+//
+//  Created by Arjun Bhatnagar on 1/17/14.
+//  Copyright (c) 2014 Productions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ SoundByte/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SoundByte
+//
+//  Created by Arjun Bhatnagar on 1/17/14.
+//  Copyright (c) 2014 Productions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
